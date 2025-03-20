5aec8ecfcd767b2f7dfd48f5e4d2ce9d6e733b2b
nguyenhuyttns <huy2010200320102003@gmail.com>
2025-03-20 23:15:02 +0700

    create stuctures

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+
+
+
+app.use(cors());
+app.options('*',cors());
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+
+//Routers
+const productsRouter = require('./routes/products');
+const categoriesRoutes = require('./routes/categories');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/products`, productsRouter);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+
+//database
+mongoose
+    .connect(process.env.CONNECTION_STRING, {})
+    .then(() => {
+        console.log('Database Connection is ready...');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(3000, () => {
+    console.log(api);
+    console.log('server is running http://localhost:3000');
+});
