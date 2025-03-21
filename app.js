506c229208c5b893daa193512cc5e55167e7a734
nguyenhuyttns <huy2010200320102003@gmail.com>
2025-03-22 00:45:24 +0700

    update order api

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 require('dotenv/config');
 
 
@@ -15,6 +17,8 @@ app.options('*',cors());
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
+app.use(authJwt());
+// app.use(errorHandler);
 
 
 //Routers
