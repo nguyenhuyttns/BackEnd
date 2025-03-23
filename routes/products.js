4aec6354dcbb2233aba5e35df52258b577799fab
nguyenhuyttns <huy2010200320102003@gmail.com>
2025-03-23 15:33:13 +0700

    update order api

--- routes/products.js
+++ routes/products.js
@@ -1,34 +1,59 @@
-const {Product} = require('../models/product');
+const { Product } = require('../models/product');
 const { Category } = require('../models/category');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
 
-router.get(`/`, async (req, res) =>{
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if (isValid) {
+            uploadError = null;
+        }
+        cb(uploadError, 'public/uploads');
+    },
+    filename: function (req, file, cb) {
+        const fileName = file.originalname.split(' ').join('-');
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${extension}`);
+    },
+});
+
+const uploadOptions = multer({ storage: storage });
+
+router.get(`/`, async (req, res) => {
     //http://localhost:3000/api/v1/products?categories=67dc44aa1c5d49e3e6033647 (find by category)
     let filter = {};
-    if(req.query.categories)
-    {
-         filter = {category: req.query.categories.split(',')}
+    if (req.query.categories) {
+        filter = { category: req.query.categories.split(',') };
     }
 
     const productList = await Product.find(filter).populate('category');
 
-    if(!productList) {
-        res.status(500).json({success: false})
-    } 
+    if (!productList) {
+        res.status(500).json({ success: false });
+    }
     res.send(productList);
-})
+});
 
-router.get(`/:id`, async (req, res) =>{
+router.get(`/:id`, async (req, res) => {
     //http://localhost:3000/api/v1/products/67dcea5740cea065c85174aa (find by id)
     const product = await Product.findById(req.params.id).populate('category');
 
-    if(!product) {
-        res.status(500).json({success: false})
-    } 
+    if (!product) {
+        res.status(500).json({ success: false });
+    }
     res.send(product);
-})
+});
 
 router.get(`/get/count`, async (req, res) => {
     //http://localhost:3000/api/v1/products/get/count (cout number of product)
@@ -36,36 +61,39 @@ router.get(`/get/count`, async (req, res) => {
         const productCount = await Product.countDocuments();
 
         res.status(200).send({
-            productCount: productCount
+            productCount: productCount,
         });
     } catch (error) {
         res.status(500).json({ success: false, error: error.message });
     }
 });
 
-router.get(`/get/featured/:count`, async (req, res) =>{
+router.get(`/get/featured/:count`, async (req, res) => {
     //http://localhost:3000/api/v1/products/get/featured/5 (display the product is displayed)
-    const count = req.params.count ? req.params.count : 0
-    const products = await Product.find({isFeatured: true}).limit(+count);
+    const count = req.params.count ? req.params.count : 0;
+    const products = await Product.find({ isFeatured: true }).limit(+count);
 
-    if(!products) {
-        res.status(500).json({success: false})
-    } 
+    if (!products) {
+        res.status(500).json({ success: false });
+    }
     res.send(products);
-})
+});
 
-
-
-router.post(`/`, async (req, res) =>{
-    //add a product
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
+    //http://localhost:3000/api/v1/products (using form data in postman)
     const category = await Category.findById(req.body.category);
-    if(!category) return res.status(400).send('Invalid Category')
+    if (!category) return res.status(400).send('Invalid Category');
 
+    const file = req.file;
+    if (!file) return res.status(400).send('No image in the request');
+
+    const fileName = file.filename;
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
         richDescription: req.body.richDescription,
-        image: req.body.image,
+        image: `${basePath}${fileName}`, // "http://localhost:3000/public/upload/image-2323232"
         brand: req.body.brand,
         price: req.body.price,
         category: req.body.category,
@@ -73,23 +101,22 @@ router.post(`/`, async (req, res) =>{
         rating: req.body.rating,
         numReviews: req.body.numReviews,
         isFeatured: req.body.isFeatured,
-    })
+    });
 
     product = await product.save();
 
-    if(!product) 
-    return res.status(500).send('The product cannot be created')
+    if (!product) return res.status(500).send('The product cannot be created');
 
     res.send(product);
-})
+});
 
-router.put('/:id',async (req, res)=> {
+router.put('/:id', async (req, res) => {
     //edit the product
-    if(!mongoose.isValidObjectId(req.params.id)) {
-       return res.status(400).send('Invalid Product Id')
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send('Invalid Product Id');
     }
     const category = await Category.findById(req.body.category);
-    if(!category) return res.status(400).send('Invalid Category')
+    if (!category) return res.status(400).send('Invalid Category');
 
     const product = await Product.findByIdAndUpdate(
         req.params.id,
@@ -106,26 +133,65 @@ router.put('/:id',async (req, res)=> {
             numReviews: req.body.numReviews,
             isFeatured: req.body.isFeatured,
         },
-        { new: true}
-    )
+        { new: true }
+    );
 
-    if(!product)
-    return res.status(500).send('the product cannot be updated!')
+    if (!product) return res.status(500).send('the product cannot be updated!');
 
     res.send(product);
-})
+});
 
-router.delete('/:id', (req, res)=>{
-    //http://localhost:3000/api/v1/products/67dc2df1e9f9a61e71402b42
-    Product.findByIdAndDelete(req.params.id).then(product =>{
-        if(product) {
-            return res.status(200).json({success: true, message: 'the product is deleted!'})
-        } else {
-            return res.status(404).json({success: false , message: "product not found!"})
+router.put(
+    //http://localhost:3000/api/v1/products/gallery-images/67df90da28fad50fcf12f36f (insert form data more images instead image)
+    '/gallery-images/:id',
+    uploadOptions.array('images', 10),
+    async (req, res) => {
+        if (!mongoose.isValidObjectId(req.params.id)) {
+            return res.status(400).send('Invalid Product Id');
         }
-    }).catch(err=>{
-       return res.status(500).json({success: false, error: err}) 
-    })
-})
+        const files = req.files;
+        let imagesPaths = [];
+        const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
 
-module.exports =router;
+        if (files) {
+            files.map((file) => {
+                imagesPaths.push(`${basePath}${file.filename}`);
+            });
+        }
+
+        const product = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                images: imagesPaths,
+            },
+            { new: true }
+        );
+
+        if (!product)
+            return res.status(500).send('the gallery cannot be updated!');
+
+        res.send(product);
+    }
+);
+
+router.delete('/:id', (req, res) => {
+    //http://localhost:3000/api/v1/products/67dc2df1e9f9a61e71402b42
+    Product.findByIdAndDelete(req.params.id)
+        .then((product) => {
+            if (product) {
+                return res.status(200).json({
+                    success: true,
+                    message: 'the product is deleted!',
+                });
+            } else {
+                return res
+                    .status(404)
+                    .json({ success: false, message: 'product not found!' });
+            }
+        })
+        .catch((err) => {
+            return res.status(500).json({ success: false, error: err });
+        });
+});
+
+module.exports = router;
